c1ed48d18bbb19f3d118570c995c99f8da4d2358
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2019-02-14 23:20:24 -0600

    hex to rgb endpoint

--- ccolors.py
+++ ccolors.py
@@ -5,3 +5,8 @@ def random_rgb():
     g = randint(0, 256)
     b = randint(0, 256)
     return (r, g, b)
+
+
+def rgb_to_hex(r, g, b):
+    return f"#{r:02x}{g:02x}{b:02x}"
+
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 """Colors API, utilities for color lovers"""
 import hug
-import ccolors
+from ccolors import random_rgb, rgb_to_hex
 @hug.get()
+
 def random():
     """Returns a random color."""
-    (r,g,b) = ccolors.random_rgb()
+    (r,g,b) = random_rgb()
     return {
-        "hex": f"#{r:02x}{g:02x}{b:02x}",
+        "hex": rgb_to_hex(r, g, b),
         "rgb": {
             "r": r,
             "g": g,
             "b": b,
         }
-        }
+    }
+
+@hug.get(examples="r=100&g=200&b=55")
+def to_hex(r:int, g:int, b:int):
+    return {"hex": rgb_to_hex(r, g, b)}
