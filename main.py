704275ba2e5acfac876cddb0e5cfb131eb180155
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2019-02-15 23:39:01 -0600

    using hug data types

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ def random():
     }
 
 @hug.get(examples="r=100&g=200&b=55")
-def to_hex(r:int, g:int, b:int):
+def to_hex(r: hug.types.in_range(0, 255), g: hug.types.in_range(0, 255), b: hug.types.in_range(0, 255)):
     return {"hex": rgb_to_hex(r, g, b)}
 
 
 @hug.get(examples=("hex=DD00FF", "hex=000","hex=123456"))
-def to_rgb(hex: str):
+def to_rgb(hex: hug.types.text):
     (r, g, b) = hex_to_rgb(hex)
     return {
         "rgb": {
